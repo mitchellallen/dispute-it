251928767ce0cb34f331574da43b30a2503f4b2f
mitchell.allen78 <mitchell.allen78@gmail.com>
2025-12-20 04:13:28 +0000

    Update Maps API key to match Google Cloud credentials

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   // This forces the keys to be available to the app
   env: {
-    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: 'AIzaSyDBjP46Cq1kordOLCACwkimWQqWoKhJci1E',
+    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: 'AIzaSyCo5gf3ddPWjxjAXnNuulMmrtkh3zNpSyM',
     NEXT_PUBLIC_GEMINI_API_KEY: 'AIzaSyBk5TFKiq0j1iRN7svynIG6QrfLF4-fnBY',
   },
   images: {
